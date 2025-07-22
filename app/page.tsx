42e43dc5762da218c4b34f455a0459dab4b829ce
sujalpatelcoder <sujal7455@gmail.com>
2025-07-22 23:37:39 +0530

    navbar created

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="w-full mx-20 py-20 h-full">
-      <Button>Test</Button>
+    <div className="w-full h-full">
+      <h1>Hello</h1>
     </div>
   );
 }
