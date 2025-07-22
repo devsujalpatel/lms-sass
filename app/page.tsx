bd210d51ec2433ac2fabe35264cca6cb5f77a642
sujalpatelcoder <sujal7455@gmail.com>
2025-07-22 23:15:41 +0530

    all the routes added

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="w-full mx-20 h-full">
-      <h1 className="text-4xl underline">Welcome to sass app</h1>
-      <Button variant={"destructive"}>Let's get started</Button>
+    <div className="w-full mx-20 py-20 h-full">
+      <Button>Test</Button>
     </div>
   );
 }
